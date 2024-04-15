8b59a9b63ee560407832cfd0bf7604fcc0fb6c78
Sean Matthew <s.matthew.english@gmail.com>
2024-04-15 00:47:58 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
-pub struct ThreadPool;
+use std::{
+    fmt, 
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
+
+pub struct ThreadPool {
+    //threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
+}
+
+type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-
-    //pub fn build(size: usize) -> Result<ThreadPool, PoolCreationError> {}
 
     /// Create a new ThreadPool.
     /// 
@@ -14,20 +24,53 @@ impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
-        ThreadPool
-    }
+        let (sender, receiver) = mpsc::channel();
 
-    
+        let receiver = Arc::new(Mutex::new(receiver));
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            // create some threads and store them in the vector
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
+        }
+
+        ThreadPool { workers, sender }
+    }
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
         {
+            let job = Box::new(f);
 
+            self.sender.send(job).unwrap();
         }
 }
 
-#[derive(Debug)]
-pub enum PoolCreationError {
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
 
+impl Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+        let thread = thread::spawn(move || loop {
+            let job = receiver.lock().unwrap().recv().unwrap();
+
+            println!("Worker {id} got a job; executing.");
+
+            job();
+        });
+
+        Worker { id, thread }
+    }
+}
+
+pub struct PoolCreationError;
+
+impl fmt::Display for PoolCreationError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "An Error Occured, Please Try Again!")
+    }
 }
