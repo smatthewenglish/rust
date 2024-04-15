3c511f08a53f080faa5bae207a83c180eb3fd1d5
Sean Matthew <s.matthew.english@gmail.com>
2024-04-15 01:39:05 -0400

    update

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,13 @@ use std::{
 };
 
 pub struct ThreadPool {
-    //threads: Vec<thread::JoinHandle<()>>,
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-
     /// Create a new ThreadPool.
     /// 
     /// The size is the number of threads in the pool.
@@ -31,11 +29,13 @@ impl ThreadPool {
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            // create some threads and store them in the vector
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        ThreadPool { 
+            workers, 
+            sender: Some(sender), 
+        }
     }
 
     pub fn execute<F>(&self, f: F)
@@ -44,26 +44,50 @@ impl ThreadPool {
         {
             let job = Box::new(f);
 
-            self.sender.send(job).unwrap();
+            self.sender.as_ref().unwrap().send(job).unwrap();
         }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
+    }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            let message = receiver.lock().unwrap().recv();
 
-            println!("Worker {id} got a job; executing.");
-
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down.");
+                    break;
+                }
+            }
         });
 
-        Worker { id, thread }
+        Worker { 
+            id, 
+            thread: Some(thread),
+        }
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,20 +5,16 @@ use std::{
     thread,
     time::Duration,
 };
-//use threadpool::ThreadPool;
 use hello::ThreadPool;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming() {
+    //for stream in listener.incoming().take(2) {
+        for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        //println!("Connection established!");
-        //handle_connection(stream);
-
-        //thread::spawn(|| {
         pool.execute(|| {
             handle_connection(stream);
         });
@@ -26,30 +22,46 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&mut stream);
 
-    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    //let buf_reader = BufReader::new(&mut stream);
+    //let request_line = buf_reader.lines().next().unwrap().unwrap();
+    // let (status_line, filename) = match &request_line[..] {
+    //     "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
+    //     "GET /sleep HTTP/1.1" => {
+    //         thread::sleep(Duration::from_secs(5));
+    //         ("HTTP/1.1 200 OK", "hello.html")
+    //     }
+    //     _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    // };
 
-    //let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
-        //("HTTP/1.1 200 OK", "hello.html")
-    //} else {
-        //("HTTP/1.1 404 NOT FOUND", "404.html")
-    //};
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
 
-    let (status_line, filename) = match &request_line[..] {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-        "GET /sleep HTTP/1.1" => {
-            thread::sleep(Duration::from_secs(5));
-            ("HTTP/1.1 200 OK", "hello.html")
-        }
-        _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1|r|n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(5));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
-    let length = contents.len();
+    
+    //let length = contents.len();
+    //let response = 
+    //    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
-    let response = 
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
     
     stream.write_all(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
