4b294c61791eb4d300c8a9dd837e1e3bd55b1e79
Sean Matthew <s.matthew.english@gmail.com>
2024-04-15 12:39:46 -0400

    ~

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs,
-    io::{prelude::*, BufReader},
+    io::prelude::*,
     net::{TcpListener, TcpStream},
     thread,
     time::Duration,
@@ -11,8 +11,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool = ThreadPool::new(4);
 
-    //for stream in listener.incoming().take(2) {
-        for stream in listener.incoming() {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         pool.execute(|| {
@@ -22,17 +21,6 @@ fn main() {
 }
 
 fn handle_connection(mut stream: TcpStream) {
-
-    //let buf_reader = BufReader::new(&mut stream);
-    //let request_line = buf_reader.lines().next().unwrap().unwrap();
-    // let (status_line, filename) = match &request_line[..] {
-    //     "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", "hello.html"),
-    //     "GET /sleep HTTP/1.1" => {
-    //         thread::sleep(Duration::from_secs(5));
-    //         ("HTTP/1.1 200 OK", "hello.html")
-    //     }
-    //     _ => ("HTTP/1.1 404 NOT FOUND", "404.html"),
-    // };
 
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
@@ -50,10 +38,6 @@ fn handle_connection(mut stream: TcpStream) {
     };
 
     let contents = fs::read_to_string(filename).unwrap();
-    
-    //let length = contents.len();
-    //let response = 
-    //    format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     let response = format!(
         "{}\r\nContent-Length: {}\r\n\r\n{}",
